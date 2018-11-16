10a9125445fbbb4a842c6acfa10f09ecb4f3663a
Andres Hernandez Zamora <a301540@uach.mx>
2018-11-16 14:23:44 -0700

    Modelo de Proyecto

--- models/proyect.js
+++ models/proyect.js
@@ -9,12 +9,11 @@ const schema = Schema({
   _description:String,
   _scrumMaster:String,
   _owner:String,
-  _team:Number
+  _team:String
 });
 
 class Proyect{
  constructor(name,dueDate,startDate,description,scrumMaster,owner,team){
-   _fullName=fullName;
    _name=name;
    _dueDate=dueDate;
    _startDate=startDate;
@@ -24,24 +23,48 @@ class Proyect{
    _team=team;
  }
 
- get fullName(){
-   return this._fullName;
- }
- set fullName(v){
-   this._fullName=v;
- }
- get birthdayDate(){
-   return this._birthdayDate;
- }
- set birthdayDate(v){
-   this._birthdayDate=v;
- }
- get role(){
-   return this._role;
- }
- set role(v){
-   this._role=v;
- }
+  get fullName(){
+    return this._name;
+  }
+  set fullName(v){
+    this._name=v;
+  }
+  get dueDate(){
+    return this._dueDate;
+  }
+  set dueDate(v){
+    this._dueDate=v;
+  }
+  get startDate(){
+    return this._startDate;
+  }
+  set startDate(v){
+    this._startDate=v;
+  }
+  get description(){
+    return this._description;
+  }
+  set description(v){
+    this._description=v;
+  }
+  get scrumMaster(){
+    return this ._scrumMaster;
+  }
+  set scrumMaster(v){
+    this._scrumMaster=v;
+  }
+  get owner(){
+    return this._owner;
+  }
+  set owner(v){
+    this._owner=v;
+  }
+   get team(){
+     return this._team;
+   }
+   set team(v){
+     this._team=v;
+   }
 
 }
 schema.plugin(mongoosePaginate);
