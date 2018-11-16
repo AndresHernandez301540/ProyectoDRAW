7056c8253fd539bbff4a90a852f80cc12b70a493
Andres Hernandez Zamora <a301540@uach.mx>
2018-11-16 14:38:19 -0700

    Controlador de Usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,109 @@
 const express = require('express');
+const User= require('../models/user');
+const {validationResult}=require('express-validator/check');
 
 function createUser(req, res, next){
-  res.send(`Nombre completo - ${req.params.name}
-    Fecha de nacimiento - ${req.params.date}
-    Rol - ${req.params.role}`);
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(422).json({ // El return rompe la funcion y ya no se ejecuta lo demas
+        errors:errors.array()
+      });
+    }
+
+    let user = new User({
+      _fullName:req.body.fullName,
+      _birthdayDate:req.body.birthdayDate,
+      _role:req.body.role
+    });
+    user.save()
+        .then((obj)=>{
+          res.status(200).json({
+            errors:[],
+            data:obj
+          });
+        })
+        .catch((err)=>{
+            res.status(500).json({
+              errors:[{message:'Algo salio mal'}],
+              data:[]
+            });
+        });
 };
 
 function indexUser(req, res, next){
-  let name = req.params.name ? req.params.name :'Sin nombre';
-  res.render('users/list',{name:req.params.name});
+  User.findById(req.params.id)
+      .then((obj)=>{
+        res.status(200).json({
+          errors:[],
+          data:obj
+        });
+      })
+      .catch((err)=>{
+        res.status(500).json({
+          errors:[{message:'Algo salio mal'}],
+          data:[]
+      });
+    });
 };
 
 function listUser(req, res, next){
-  res.send(`Nombre completo ${req.params.name}`);
+  let page=req.params.page ? req.params.page : 1;
+
+  const options = {
+    page:page,
+    limit:1,
+    select :'_fullName _birthdayDate _role'
+  };
+  User.paginate({},options)
+  .then((objects)=>{
+    res.status(200).json({
+      errors:[],
+      data:objects
+    });
+  }).catch((err)=>{
+    res.status(500).json({
+      errors:[{message:'Algo salio mal'}],
+      data:[]
+    });
+  });
 };
 
 function updateUser(req, res, next){
-  res.send(`El usuario ${req.params.name} ha sido actualizado `);
+  User.findById(req.params.id)
+  .then((obj)=>{
+    obj.fullName=req.body.fullName ? req.body.fullName : obj.fullName;
+    obj.birthdayDate=req.body.birthdayDate ? req.body.birthdayDate : obj.birthdayDate;
+    obj.role=req.body.role ? req.body.role : obj.role;
+    obj.save()
+    .then((obj)=>{
+        console.log("Todo bien");
+    }).catch((err)=>{
+      res.status(500).json({
+        errors:[{message:'Algo salio mal en la actualización'}],
+        data:[]
+    });
+
+  });
+  }).catch((err)=>{
+
+  });
+
 };
 
 function deleteUser(req,res,next){
-  res.send(`El usuario ${req.params.name} ha sido eliminado`);
+  User.remove({_id: req.params.id})
+  .then((obj)=>{
+    res.status(200).json({
+      errors:[],
+      data:obj
+    });
+  })
+  .catch((err)=>{
+    res.status(500).json({
+      errors:[{message:'Algo salio mal'}],
+      data:[]
+    });
+  });
 };
 
 module.exports={
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const usersController=require('../controllers/usersController');
+const { check, body, params} = require('express-validator/check');
 
 const userController=require('../controllers/userController');
 
 /* GET users listing. */
+router.post('/',[
+  body('fullName').not().isEmpty(),
+  body('birthdayDate').not().isEmpty(),
+  body('role').not().isEmpty(),
+],usersController.createUser);
+
 router.get('/:name?',userController.indexUser);
 router.get('/',userController.listUser);
-router.post('/:name/:date/:role',userController.createUser);
+
 router.put('/:name',userController.updateUser);
 router.delete('/:name',userController.deleteUser);
 
