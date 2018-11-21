5331a7ea4e4d009a1ca3eee59bb4c338d3f11ece
Andres Hernandez Zamora <a301540@uach.mx>
2018-11-20 21:03:20 -0700

    Correccion con mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const usersController=require('../controllers/userController');
+const userController=require('../controllers/userController');
 const { check, body, params} = require('express-validator/check');
 
-const userController=require('../controllers/userController');
 
 /* GET users listing. */
 router.post('/',[
   body('fullName').not().isEmpty(),
   body('birthdayDate').not().isEmpty(),
   body('role').not().isEmpty(),
-],usersController.createUser);
+],userController.createUser);
 
 router.get('/:name?',userController.indexUser);
 router.get('/',userController.listUser);
